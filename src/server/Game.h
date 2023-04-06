6619b33caf75183963b0f0d5b87b93b57400b362
Youka4 <dim.youka4@gmail.com>
2023-04-06 16:35:14 +0300

    Logic_update
    Update to last youka branch fixes (Full_consol_app)

--- src/server/Game.h
+++ src/server/Game.h
@@ -1,32 +1,51 @@
 #ifndef GAME_H
 #define GAME_H
 
-#include "../rename_later.h"
 #include "../client/Client.h"
+#include "../rename_later.h"
 #include "DataBase_connector.h"
 
 namespace client {
-    struct Client;
+struct Client;
 }
+
+enum Which_turn { Preflop, Flop, Turn, River };
+
+struct Blinds {
+    int big_blind = 10;
+    int small_blind = 5;
+};
 
 struct Card {
 private:
     enum class Value {
-        Two, Three,Four,Five,Six,
-        Seven,Eight,Nine,Ten,Jack,Queen,King,Ace
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace
     };
     enum class Suit { DIAMONDS, HEARTS, CLUBS, SPADES };
     Suit suit;
     Value value;
     int index;
+
 public:
     explicit Card(int num) : index(num) {
         static const std::vector<Value> values_of_cards = {
-                Value::Two,   Value::Three, Value::Four, Value::Five, Value::Six,
-                Value::Seven, Value::Eight, Value::Nine, Value::Ten,  Value::Jack,
-                Value::Queen, Value::King,  Value::Ace};
+            Value::Two,   Value::Three, Value::Four, Value::Five, Value::Six,
+            Value::Seven, Value::Eight, Value::Nine, Value::Ten,  Value::Jack,
+            Value::Queen, Value::King,  Value::Ace};
         static const std::vector<Suit> suits_of_cards = {
-                Suit::HEARTS, Suit::DIAMONDS, Suit::SPADES, Suit::CLUBS};
+            Suit::HEARTS, Suit::DIAMONDS, Suit::SPADES, Suit::CLUBS};
         auto num_of_value = num / 4;
         value = values_of_cards[num_of_value];
         suit = suits_of_cards[num % 4];
@@ -36,36 +55,49 @@ public:
         return index;
     }
 
-    std::string ValueToString() noexcept
-    {
+    std::string ValueToString() noexcept {
         auto e = this->value;
-        switch (e)
-        {
-            case Value::Two : return "Two";
-             case Value::Three : return "Three";
-             case Value::Four : return "Four";
-             case Value::Five : return "Five";
-             case Value::Six : return "Six";
-              case  Value::Seven : return "Seven";
-              case Value::Eight : return "Eight";
-              case Value::Nine : return "Nine";
-              case Value::Ten : return "Ten";
-              case  Value::Jack : return "Jack";
-              case  Value::Queen : return "Queen";
-              case Value::King : return "King";
-              case  Value::Ace : return "Ace";
+        switch (e) {
+            case Value::Two:
+                return "Two";
+            case Value::Three:
+                return "Three";
+            case Value::Four:
+                return "Four";
+            case Value::Five:
+                return "Five";
+            case Value::Six:
+                return "Six";
+            case Value::Seven:
+                return "Seven";
+            case Value::Eight:
+                return "Eight";
+            case Value::Nine:
+                return "Nine";
+            case Value::Ten:
+                return "Ten";
+            case Value::Jack:
+                return "Jack";
+            case Value::Queen:
+                return "Queen";
+            case Value::King:
+                return "King";
+            case Value::Ace:
+                return "Ace";
         }
     }
 
-    std::string SuitToString() noexcept
-    {
+    std::string SuitToString() noexcept {
         auto e = this->suit;
-        switch (e)
-        {
-            case Suit::HEARTS : return "of HEARTS";
-            case Suit::DIAMONDS : return "of DIAMONDS";
-            case Suit::SPADES : return "of SPADES";
-            case Suit::CLUBS : return "of CLUBS";
+        switch (e) {
+            case Suit::HEARTS:
+                return "of HEARTS";
+            case Suit::DIAMONDS:
+                return "of DIAMONDS";
+            case Suit::SPADES:
+                return "of SPADES";
+            case Suit::CLUBS:
+                return "of CLUBS";
         }
     }
 
@@ -79,7 +111,12 @@ public:
 namespace server {
 struct Game {
 private:
+    Which_turn current_turn = Which_turn::Preflop;
     std::vector<client::Client> players;
+    std::vector<client::client> round_players;
+    int button;
+    int last_player;
+    Blinds blinds;
     std::map<client::Client, std::pair<Card, Card>> cards_enum;
     std::map<client::Client, int> balance;
     std::vector<int> available_cards;
@@ -89,15 +126,24 @@ private:
 public:
     explicit Game(std::vector<client::Client> lobby);
     void bets();
+    int next_position(int position);
     void preflop();
     void flop();
     void make_a_bet(const client::Client &player, int bet_amount);
     void turn();
     void river();
     Card get_enum_card();
-
     void print_cards();
     void who_won();
+    void new_round();
+
+    template <typename T>
+    void increase_iterator(T &iterator) {
+        iterator++;
+        if (iterator == round_players.end()) {
+            iterator = round_players.begin();
+        }
+    }
 };
 
 }  // namespace server
--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -2,24 +2,21 @@
 
 namespace server {
 
-    void Server::add(const client::Client &client) {
-        players.push(client);
-    }
+void Server::add(const client::Client &client) {
+    players.push(client);
+}
 
-    void Server::start_game() {
-        std::vector<client::Client> lobby;
-        for (int i = 0; i < 5; i++) {
-            if (!players.empty()) {
-                auto player = (players.front());
-                players.pop();
-                lobby.emplace_back(player);
-            }
+void Server::start_game() {
+    std::vector<client::Client> lobby;
+    for (int i = 0; i < 5; i++) {
+        if (!players.empty()) {
+            auto player = (players.front());
+            players.pop();
+            lobby.emplace_back(player);
         }
-        games.emplace_back(lobby);
-        auto &our_game = games.back();
-        our_game.preflop();
-        our_game.flop();
-        our_game.turn();
-        our_game.river();
     }
-}  // namespace Server
+    games.emplace_back(lobby);
+    auto &our_game = games.back();
+    our_game.preflop();
+}
+}  // namespace server
