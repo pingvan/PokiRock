f20a75768e5b9f2052c6d1cb132f74e2e3427412
ilia <ilia.anikeev@gmail.com>
2023-05-24 17:52:48 +0300

    clear repo

--- src/gui/cmake-build-debug/CMakeFiles/PokiRock_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/PokiRock_autogen"
-  "PokiRock_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/PokiRock_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
